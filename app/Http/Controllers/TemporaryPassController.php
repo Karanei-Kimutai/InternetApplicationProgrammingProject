cd586dddc9e3b491cd4e6fe5959d43e6080edf59
Karanei-Kimutai <karaneijunior@gmail.com>
2025-10-17 12:45:03 +0300

    Wrote sql for the dummy AMS, added the connection to config/database.php, created the UniversityMember model which we'll use to communicate with the external database, created the TemporaryPass and Admin models to cater for the code added

---
+++ app/Http/Controllers/TemporaryPassController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreTemporaryPassRequest;
+use App\Http\Requests\UpdateTemporaryPassRequest;
+use App\Models\TemporaryPass;
+
+class TemporaryPassController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTemporaryPassRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(TemporaryPass $temporaryPass)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(TemporaryPass $temporaryPass)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTemporaryPassRequest $request, TemporaryPass $temporaryPass)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(TemporaryPass $temporaryPass)
+    {
+        //
+    }
+}
